0b35229c9828bbc3d67067d9b0546ce1efaf955b
Christophe Boulanger <christophe.boulanger@imio.be>
2021-07-07 08:56:22 +0200

    Add topics field behavior
    With topics vocabulary select widget

---
+++ src/imio/smartweb/common/behaviors/topics.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.locales import SmartwebMessageFactory as _
+from plone.app.z3cform.widget import SelectFieldWidget
+from plone.autoform import directives
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import model
+from zope import schema
+from zope.interface import provider
+
+
+@provider(IFormFieldProvider)
+class ITopics(model.Schema):
+
+    model.fieldset(
+        "categorization", label=_(u"Categorization"), fields=["topics"]
+    )
+    topics = schema.List(
+        title=_(u"Topics"),
+        description=_(u"Topics"),
+        value_type=schema.Choice(vocabulary="imio.smartweb.vocabulary.Topics"),
+    )
+    directives.widget(topics=SelectFieldWidget)
