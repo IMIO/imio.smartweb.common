4a51d77be391ce0aeab6fe88b9258eb4bea82f86
Laurent Lasudry <lasudry@gmail.com>
2021-08-12 15:46:53 +0200

    Add missing imports

--- src/imio/smartweb/common/vocabularies.py
+++ src/imio/smartweb/common/vocabularies.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from imio.smartweb.locales import SmartwebMessageFactory as _
+from plone import api
+from plone.i18n.normalizer.interfaces import IIDNormalizer
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from zope.i18n.locales import locales
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
+
+import json
 
 
 class TopicsVocabularyFactory:
