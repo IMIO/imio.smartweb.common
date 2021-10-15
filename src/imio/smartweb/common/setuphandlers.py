b4b10c96d248d6c3d5e4f89d93b21b2be33391b9
Laurent Lasudry <lasudry@gmail.com>
2021-10-15 11:49:56 +0200

    Increase cookie timeout to 8 hours

--- src/imio/smartweb/common/setuphandlers.py
+++ src/imio/smartweb/common/setuphandlers.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+
+from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
@@ -14,7 +16,12 @@ class HiddenProfiles(object):
 
 def post_install(context):
     """Post install script"""
-    # Do something at the end of the installation of this package.
+    acl_users = api.portal.get_tool("acl_users")
+    session = acl_users.session
+    # don't make cookie persistent across closing the browser
+    session.cookie_lifetime = 0
+    # set cookie validity to 8 hours
+    session.timeout = 8 * 3600
 
 
 def uninstall(context):
