ba296637463e198ed68c9043ae6851461b5cbaa1
Laurent Lasudry <lasudry@gmail.com>
2022-01-12 14:18:27 +0100

    Add upgrade steps for privacy / cookies

--- src/imio/smartweb/common/upgrades.py
+++ src/imio/smartweb/common/upgrades.py
@@ -7,6 +7,6 @@ logger = logging.getLogger("imio.smartweb.common")
 PROFILEID = "profile-imio.smartweb.common:default"
 
 
-def configure_first_official_release(context):
+def reload_registry(context):
     portal_setup = api.portal.get_tool("portal_setup")
     portal_setup.runImportStepFromProfile(PROFILEID, "plone.app.registry")
