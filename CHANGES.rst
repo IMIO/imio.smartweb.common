254674096f7c462f5f5c987fc55fe1ca0ef96af0
Laurent Lasudry <lasudry@gmail.com>
2022-02-07 17:25:25 +0100

    Hide ical import related actions

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,7 +5,8 @@ Changelog
 1.0a10 (unreleased)
 -------------------
 
-- Nothing changed yet.
+- Hide ical import related actions
+  [laulaz]
 
 
 1.0a9 (2022-02-01)
--- src/imio/smartweb/common/upgrades.py
+++ src/imio/smartweb/common/upgrades.py
@@ -10,3 +10,8 @@ PROFILEID = "profile-imio.smartweb.common:default"
 def reload_registry(context):
     portal_setup = api.portal.get_tool("portal_setup")
     portal_setup.runImportStepFromProfile(PROFILEID, "plone.app.registry")
+
+
+def reload_actions(context):
+    portal_setup = api.portal.get_tool("portal_setup")
+    portal_setup.runImportStepFromProfile(PROFILEID, "actions")
