1950a5d4be1be75ba893d62521465211b71c780f
Laurent Lasudry <lasudry@gmail.com>
2022-03-31 17:08:11 +0200

    Add / improve tests

--- src/imio/smartweb/common/tests/test_local_roles.py
+++ src/imio/smartweb/common/tests/test_local_roles.py
@@ -30,6 +30,46 @@ class TestLocalRoles(unittest.TestCase):
             title="Folder",
         )
 
+    def test_sharing_page_available(self):
+        alsoProvides(self.folder, ILocalManagerAware)
+        setRoles(self.portal, TEST_USER_ID, ["Editor"])
+        transaction.commit()
+        browser = Browser(self.layer["app"])
+        browser.addHeader(
+            "Authorization",
+            "Basic %s:%s"
+            % (
+                TEST_USER_NAME,
+                TEST_USER_PASSWORD,
+            ),
+        )
+        browser.open("{}/@@sharing".format(self.folder.absolute_url()))
+        content = browser.contents
+        self.assertIn("Insufficient Privileges", content)
+
+        self.folder.manage_setLocalRoles(TEST_USER_ID, ("Local Manager",))
+        transaction.commit()
+        browser.open("{}/@@sharing".format(self.folder.absolute_url()))
+        content = browser.contents
+        self.assertIn("Can add", content)
+        self.assertIn("Can edit", content)
+        self.assertIn("Can manage locally", content)
+        self.assertIn("Can review", content)
+        self.assertIn("Can view", content)
+        self.assertNotIn("Insufficient Privileges", content)
+
+        transaction.commit()
+        browser.open("{}/@@sharing".format(self.portal.absolute_url()))
+        content = browser.contents
+        self.assertIn("Insufficient Privileges", content)
+
+    def test_delegate_sharing_permission(self):
+        self.assertTrue(_checkPermission(DelegateRoles, self.folder))
+        setRoles(self.portal, TEST_USER_ID, ["Editor"])
+        self.assertFalse(_checkPermission(DelegateRoles, self.folder))
+        setRoles(self.portal, TEST_USER_ID, ["Local Manager"])
+        self.assertTrue(_checkPermission(DelegateRoles, self.folder))
+
     def test_local_manager_in_sharing(self):
         transaction.commit()
         browser = Browser(self.layer["app"])
@@ -50,10 +90,3 @@ class TestLocalRoles(unittest.TestCase):
         browser.open("{}/@@sharing".format(self.folder.absolute_url()))
         content = browser.contents
         self.assertIn("Can manage locally", content)
-
-    def test_delegate_sharing(self):
-        self.assertTrue(_checkPermission(DelegateRoles, self.folder))
-        setRoles(self.portal, TEST_USER_ID, ["Editor"])
-        self.assertFalse(_checkPermission(DelegateRoles, self.folder))
-        setRoles(self.portal, TEST_USER_ID, ["Local Manager"])
-        self.assertTrue(_checkPermission(DelegateRoles, self.folder))
