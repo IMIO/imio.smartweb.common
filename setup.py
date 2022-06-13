326f28c67796f728d9f1224734f565ad34cddc4c
Christophe Boulanger <christophe.boulanger@imio.be>
2022-06-13 14:24:33 +0200

    Back to development: 1.0.8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.smartweb.common",
-    version="1.0.7",
+    version="1.0.8.dev0",
     description="Common utilities, vocabularies, taxonomies for imio.smartweb & co products",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
