02df2b1ac41b70ce0c6b5afddc105ccce1afb486
Laurent Lasudry <lasudry@gmail.com>
2023-04-13 17:16:12 +0200

    Blacked

--- src/imio/smartweb/common/browser/scaling.py
+++ src/imio/smartweb/common/browser/scaling.py
@@ -1,6 +1,7 @@
 from plone.namedfile.scaling import NavigationRootScaling as BaseNavigationRootScaling
 from zope.cachedescriptors.property import Lazy as lazy_property
 
+
 class NavigationRootScaling(BaseNavigationRootScaling):
     @lazy_property
     def _supports_image_scales_metadata(self):
