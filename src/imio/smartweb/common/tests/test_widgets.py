958600265dd6b20f44fcb390a7ec641eea80b39d
Rémi Dubois <remi.dubois@gmail.com>
2025-04-25 14:28:35 +0200

    fix: tests on plone 6.1

--- src/imio/smartweb/common/tests/test_widgets.py
+++ src/imio/smartweb/common/tests/test_widgets.py
@@ -2,8 +2,10 @@
 
 from imio.smartweb.common.testing import IMIO_SMARTWEB_COMMON_INTEGRATION_TESTING
 from imio.smartweb.common.widgets.select import TranslatedAjaxSelectWidget
+from importlib.metadata import version
 from plone.api import portal as portal_api
 from zope.publisher.browser import TestRequest
+
 
 import mock
 import unittest
@@ -20,42 +22,99 @@ class TestVocabulary(unittest.TestCase):
         portal_api.get_current_language = mock.Mock(return_value="fr")
         widget = TranslatedAjaxSelectWidget(self.request)
         widget.update()
-        self.assertEqual(
-            {
-                "name": None,
-                "value": "",
-                "pattern": "select2",
-                "pattern_options": {"separator": ";"},
-            },
-            widget._base_args(),
-        )
-
-        widget.vocabulary = "imio.smartweb.vocabulary.Topics"
-        self.assertEqual(
-            widget._base_args(),
-            {
-                "name": None,
-                "value": "",
-                "pattern": "select2",
-                "pattern_options": {
-                    "vocabularyUrl": "http://nohost/plone/@@getVocabulary?name=imio.smartweb.vocabulary.Topics",
-                    "separator": ";",
+        # __import__("pdb").set_trace()
+        if version("plone.app.z3cform") < "4.4.0":
+            self.assertEqual(
+                {
+                    "name": None,
+                    "value": "",
+                    "pattern": "select2",
+                    "pattern_options": {"separator": ";"},
                 },
-            },
-        )
-        widget.value = "entertainment"
-        self.assertEqual(
-            widget._base_args(),
-            {
-                "name": None,
-                "value": "entertainment",
-                "pattern": "select2",
-                "pattern_options": {
-                    "vocabularyUrl": "http://nohost/plone/@@getVocabulary?name=imio.smartweb.vocabulary.Topics",
-                    "initialValues": {
-                        "entertainment": "Activités et divertissement",
+                widget._base_args(),
+            )
+            widget.vocabulary = "imio.smartweb.vocabulary.Topics"
+            self.assertEqual(
+                widget._base_args(),
+                {
+                    "name": None,
+                    "value": "",
+                    "pattern": "select2",
+                    "pattern_options": {
+                        "vocabularyUrl": "http://nohost/plone/@@getVocabulary?name=imio.smartweb.vocabulary.Topics",
+                        "separator": ";",
                     },
-                    "separator": ";",
                 },
-            },
-        )
+            )
+            widget.value = "entertainment"
+            self.assertEqual(
+                widget._base_args(),
+                {
+                    "name": None,
+                    "value": "entertainment",
+                    "pattern": "select2",
+                    "pattern_options": {
+                        "vocabularyUrl": "http://nohost/plone/@@getVocabulary?name=imio.smartweb.vocabulary.Topics",
+                        "initialValues": {
+                            "entertainment": "Activités et divertissement",
+                        },
+                        "separator": ";",
+                    },
+                },
+            )
+        else:
+            # _base.args has been removed
+            # see https://github.com/plone/plone.app.z3cform/commit/5de30e9b117885859680dfe3861f134a667cea9c
+            self.assertEqual(
+                {
+                    "name": None,
+                    "value": None,
+                    "pattern": "select2",
+                    "pattern_options": {"separator": ";"},
+                },
+                {
+                    "name": widget.name,
+                    "value": widget.value,
+                    "pattern": widget.pattern,
+                    "pattern_options": widget.get_pattern_options(),
+                },
+            )
+            widget.vocabulary = "imio.smartweb.vocabulary.Topics"
+            self.assertEqual(
+                {
+                    "name": None,
+                    "value": None,
+                    "pattern": "select2",
+                    "pattern_options": {
+                        "vocabularyUrl": "http://nohost/plone/@@getVocabulary?name=imio.smartweb.vocabulary.Topics",
+                        "separator": ";",
+                    },
+                },
+                {
+                    "name": widget.name,
+                    "value": widget.value,
+                    "pattern": widget.pattern,
+                    "pattern_options": widget.get_pattern_options(),
+                },
+            )
+            widget.value = "entertainment"
+            self.assertEqual(
+                {
+                    "name": None,
+                    "value": "entertainment",
+                    "pattern": "select2",
+                    "pattern_options": {
+                        "vocabularyUrl": "http://nohost/plone/@@getVocabulary?name=imio.smartweb.vocabulary.Topics",
+                        "initialValues": {
+                            "entertainment": "Activités et divertissement",
+                        },
+                        "separator": ";",
+                    },
+                },
+                {
+                    "name": widget.name,
+                    "value": widget.value,
+                    "pattern": widget.pattern,
+                    "pattern_options": widget.get_pattern_options(),
+                },
+            )
