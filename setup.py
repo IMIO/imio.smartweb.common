85645bbb0cc7c3d5d1e0716d9029bb37bcacb619
boulch <christophe.boulanger@imio.be>
2025-09-10 15:00:46 +0200

    Back to development: 1.2.39

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.smartweb.common",
-    version="1.2.38",
+    version="1.2.39.dev0",
     description="Common utilities, vocabularies, taxonomies for imio.smartweb & co products",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
