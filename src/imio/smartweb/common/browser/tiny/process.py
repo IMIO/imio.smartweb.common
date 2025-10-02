aa96f08974975f4161f738adb0930eb4dc77d74a
thomlamb <lambertthomas10@gmail.com>
2025-10-02 08:58:10 +0200

    Add AI endpoint

--- src/imio/smartweb/common/browser/tiny/process.py
+++ src/imio/smartweb/common/browser/tiny/process.py
@@ -71,3 +71,59 @@ class ProcessSuggestedTitlesView(BaseIAView):
         titles = data.get("suggested_titles", [])
         new_html = "<ul>{}</ul>".format("".join(f"<li>{t}</li>" for t in titles))
         return json.dumps({"html": f"{new_html}<br />{current_html}"})
+
+
+class ProcessTextShorterView(BaseIAView):
+
+    def __call__(self):
+        self.request.response.setHeader(
+            "Content-Type", "application/json; charset=utf-8"
+        )
+        body = self.request.get("BODY", b"") or self.request.stdin.read() or b""
+        try:
+            data = json.loads(body.decode("utf-8"))
+        except Exception:
+            data = {}
+        current_html = data.get("html", "")
+        payload = {
+            "input": current_html,
+            "expansion_target": 50,
+        }
+        url = "http://127.0.0.1:8000/common/text-shorter"
+        response = requests.post(url, headers=self.headers, json=payload)
+        if response.status_code != 200:
+            return current_html
+        data = response.json()
+        if not data:
+            return current_html
+
+        new_html = data.get("result")
+        return json.dumps({"html": new_html})
+
+
+class ProcessTextImproveView(BaseIAView):
+
+    def __call__(self):
+        self.request.response.setHeader(
+            "Content-Type", "application/json; charset=utf-8"
+        )
+        body = self.request.get("BODY", b"") or self.request.stdin.read() or b""
+        try:
+            data = json.loads(body.decode("utf-8"))
+        except Exception:
+            data = {}
+        current_html = data.get("html", "")
+        payload = {
+            "input": current_html,
+            "expansion_target": 50,
+        }
+        url = "http://127.0.0.1:8000/common/text-improve"
+        response = requests.post(url, headers=self.headers, json=payload)
+        if response.status_code != 200:
+            return current_html
+        data = response.json()
+        if not data:
+            return current_html
+
+        new_html = data.get("result")
+        return json.dumps({"html": new_html})
