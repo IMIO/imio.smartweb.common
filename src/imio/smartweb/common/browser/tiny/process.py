de4b3ba8d534e004b0149cf614b7dad09056ba98
thomlamb <lambertthomas10@gmail.com>
2025-10-02 14:10:15 +0200

    Delete Title from ia btn

--- src/imio/smartweb/common/browser/tiny/process.py
+++ src/imio/smartweb/common/browser/tiny/process.py
@@ -45,35 +45,6 @@ class ProcessTextExpandView(BaseIAView):
         return json.dumps({"html": new_html})
 
 
-class ProcessSuggestedTitlesView(BaseIAView):
-    """"""
-
-    def __call__(self):
-        self.request.response.setHeader(
-            "Content-Type", "application/json; charset=utf-8"
-        )
-        body = self.request.get("BODY", b"") or self.request.stdin.read() or b""
-        try:
-            data = json.loads(body.decode("utf-8"))
-        except Exception:
-            data = {}
-        current_html = data.get("html", "")
-        payload = {
-            "input": current_html,
-            "expansion_target": 50,
-        }
-        url = f"{IPA_URL}/suggest-titles"
-        response = requests.post(url, headers=self.headers, json=payload)
-        if response.status_code != 200:
-            return current_html
-        data = response.json()
-        if not data:
-            return current_html
-        titles = data.get("suggested_titles", [])
-        new_html = "<ul>{}</ul>".format("".join(f"<li>{t}</li>" for t in titles))
-        return json.dumps({"html": f"{new_html}<br />{current_html}"})
-
-
 class ProcessTextShorterView(BaseIAView):
 
     def __call__(self):
